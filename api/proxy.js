e470a26b703bd0bf48f4e12b8a4c238e52447abd
natnael-tilahun <natnaeltilahun97@gmail.com>
2024-11-18 23:35:07 +0300

    Add vercel edge function

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    console.log("req.body: ", req.body);
     try {
+      console.log("Incoming req.body: ", req.body);
+
       const response = await fetch(
         "https://api.datalot.com/contact/create/v2",
         {
@@ -9,52 +10,16 @@ export default async function handler(req, res) {
           headers: {
             "Content-Type": "application/xml",
           },
-          body: `
-          <?xml version="1.0" encoding="UTF-8"?>
-<contact_create>
-    <access_key>p8h9l5cfwx</access_key>
-    <source_id>14860</source_id>
-    <product_id>740</product_id>
-    <campaign_id>bdc_64604_business_phone_systems</campaign_id>
-    <pass_through>1234</pass_through>
-    <contact_permission>
-        <lead_id>trustedform</lead_id>
-        <phone_explicit>1</phone_explicit>
-        <mobile_explicit>1</mobile_explicit>
-        <autodial_explicit>1</autodial_explicit>
-        <sms_explicit>1</sms_explicit>
-        <mms_explicit>1</mms_explicit>
-        <email_explicit>1</email_explicit>
-        <form_url>WEBSITELEADCAMEFROM</form_url>
-        <legal_language>LEGALLANGUAGE LEAD SAW ON FORM SUBMIT</legal_language>
-    </contact_permission>
-    <contact_info>
-        <general_info>
-            <first_name>Datalot</first_name>
-            <last_name>Smith</last_name>
-            <phone_home>4094344023</phone_home>
-            <email>kdskfjslkdffffffjdslf@aol.com</email>
-            <city>Beaumont</city>
-            <state>TX</state>
-            <zip_code>77706</zip_code>
-        </general_info>
-        <product_info>
-            <bdc_category>business_phone_systems</bdc_category>
-            <service_type>Installing new phone system</service_type>
-            <number_of_phones>1-3</number_of_phones>
-        </product_info>
-    </contact_info>
-    <test>1</test>
-</contact_create>
-
-          `, // XML sent from the frontend
+          body:
+            typeof req.body === "string" ? req.body : JSON.stringify(req.body),
         }
       );
 
       const result = await response.text();
-      console.log("result: ", result);
+      console.log("API Response: ", result);
       res.status(response.status).send(result);
     } catch (error) {
+      console.error("Proxy Error:", error);
       res.status(500).send(`Error: ${error.message}`);
     }
   } else {
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -23,12 +23,10 @@ function Form() {
   };
 
   const handleSubmit = async (e) => {
-    setLoading(true);
     e.preventDefault();
-    console.log(window.location.href);
 
-    // Create XML string
-    const xmlData = `
+    try {
+      const xmlData = `
 <?xml version="1.0" encoding="UTF-8"?>
 <contact_create>
     <access_key>p8h9l5cfwx</access_key>
@@ -65,62 +63,20 @@ function Form() {
     </contact_info>
     <test>1</test>
 </contact_create>
-    `;
+      `;
 
-    console.log("xmlData: ", xmlData);
-
-    try {
       const response = await fetch("/api/proxy", {
         method: "POST",
         headers: {
           "Content-Type": "application/xml",
         },
-        body: `
-        <?xml version="1.0" encoding="UTF-8"?>
-<contact_create>
-    <access_key>p8h9l5cfwx</access_key>
-    <source_id>14860</source_id>
-    <product_id>740</product_id>
-    <campaign_id>bdc_64604_business_phone_systems</campaign_id>
-    <pass_through>1234</pass_through>
-    <contact_permission>
-        <lead_id>trustedform</lead_id>
-        <phone_explicit>1</phone_explicit>
-        <mobile_explicit>1</mobile_explicit>
-        <autodial_explicit>1</autodial_explicit>
-        <sms_explicit>1</sms_explicit>
-        <mms_explicit>1</mms_explicit>
-        <email_explicit>1</email_explicit>
-        <form_url>WEBSITELEADCAMEFROM</form_url>
-        <legal_language>LEGALLANGUAGE LEAD SAW ON FORM SUBMIT</legal_language>
-    </contact_permission>
-    <contact_info>
-        <general_info>
-            <first_name>Datalot</first_name>
-            <last_name>Smith</last_name>
-            <phone_home>4094344023</phone_home>
-            <email>kdskfjslkdffffffjdslf@aol.com</email>
-            <city>Beaumont</city>
-            <state>TX</state>
-            <zip_code>77706</zip_code>
-        </general_info>
-        <product_info>
-            <bdc_category>business_phone_systems</bdc_category>
-            <service_type>Installing new phone system</service_type>
-            <number_of_phones>1-3</number_of_phones>
-        </product_info>
-    </contact_info>
-    <test>1</test>
-</contact_create>
-        `,
+        body: xmlData,
       });
 
       const result = await response.text();
-      console.log("API Response:", result);
-      setLoading(false);
+      console.log("Response:", result);
     } catch (error) {
-      console.error("Error submitting form:", error.message);
-      setLoading(false);
+      console.error("Error:", error);
     }
   };
 
