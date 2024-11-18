d5ac5fed03f307218ee37f1cdd0bd69305b7010c
natnael-tilahun <natnaeltilahun97@gmail.com>
2024-11-18 23:39:34 +0300

    Add vercel edge function

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
-      console.log("Incoming req.body: ", req.body);
+      console.log("Incoming req.body: ", req);
 
       const response = await fetch(
         "https://api.datalot.com/contact/create/v2",
